a238466d8af55730beb0ca8806f9f5ec61170c22
Nikita <tt.nikitk@mail.ru>
2023-05-05 16:57:04 +0400

    Add normal readme

--- parser/parse_tsu.py
+++ parser/parse_tsu.py
@@ -20,9 +20,9 @@ implicitly_wait = 10
 @contextlib.contextmanager
 def init_browser():
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
